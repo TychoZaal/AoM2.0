62089e9c695f7ec1f8243f3943ba4d002f783b1a
TychoDesktop <TychoZaal@gmail.com>
2020-08-20 00:43:49 +0200

    Camera angle, movement and zooming working

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,124 @@
+﻿#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
+#define USE_INPUT_SYSTEM
+    using UnityEngine.InputSystem;
+    using UnityEngine.InputSystem.Controls;
+#endif
+
+using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        class CameraState
+        {
+            public float yaw;
+            public float pitch;
+            public float roll;
+            public float x;
+            public float y;
+            public float z;
+
+            public void SetFromTransform(Transform t)
+            {
+                pitch = t.eulerAngles.x;
+                yaw = t.eulerAngles.y;
+                roll = t.eulerAngles.z;
+                x = t.position.x;
+                y = t.position.y;
+                z = t.position.z;
+            }
+
+            public void Translate(Vector3 translation)
+            {
+                Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
+
+                x += rotatedTranslation.x;
+                y += rotatedTranslation.y;
+                z += rotatedTranslation.z;
+            }
+
+            public void LerpTowards(CameraState target, float positionLerpPct, float rotationLerpPct)
+            {
+                yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
+                pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
+                roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
+
+                x = Mathf.Lerp(x, target.x, positionLerpPct);
+                y = Mathf.Lerp(y, target.y, positionLerpPct);
+                z = Mathf.Lerp(z, target.z, positionLerpPct);
+            }
+
+            public void UpdateTransform(Transform t)
+            {
+                t.eulerAngles = new Vector3(pitch, yaw, roll);
+                t.position = new Vector3(x, y, z);
+            }
+        }
+
+        CameraState m_TargetCameraState = new CameraState();
+        CameraState m_InterpolatingCameraState = new CameraState();
+
+        [Header("Movement Settings")]
+        [Tooltip("Exponential boost factor on translation, controllable by mouse wheel.")]
+        public float cameraSpeed = 3.5f;
+
+        [Tooltip("Time it takes to interpolate camera position 99% of the way to the target."), Range(0.001f, 1f)]
+        public float positionLerpTime = 0.2f;
+
+        [Tooltip("Time it takes to interpolate camera rotation 99% of the way to the target."), Range(0.001f, 1f)]
+        public float rotationLerpTime = 0.01f;
+
+        public float edgeSizeX = 1.0f, edgeSizeY = 1.0f;
+
+        void OnEnable()
+        {
+            m_TargetCameraState.SetFromTransform(transform);
+            m_InterpolatingCameraState.SetFromTransform(transform);
+        }
+
+        Vector3 GetInputTranslationDirection()
+        {
+            Vector3 direction = new Vector3();
+            if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y > Screen.height - edgeSizeY && Input.mousePosition.y < Screen.height)
+            {
+                direction += Vector3.forward;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y < edgeSizeY && Input.mousePosition.y > 0)
+            {
+                direction += Vector3.back;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x < edgeSizeX && Input.mousePosition.x > 0)
+            {
+                direction += Vector3.left;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x > Screen.width - edgeSizeX && Input.mousePosition.x < Screen.width)
+            {
+                direction += Vector3.right;
+            }
+            return direction;
+        }
+
+        void Update()
+        {
+            Vector3 translation = Vector3.zero;
+
+            // Translation
+            translation = GetInputTranslationDirection() * Time.deltaTime;
+
+            // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
+            translation *= Mathf.Pow(2.0f, cameraSpeed);
+
+            m_TargetCameraState.Translate(translation);
+
+            // Framerate-independent interpolation
+            // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
+            var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
+            var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
+            m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+
+            m_InterpolatingCameraState.UpdateTransform(transform);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Camera/ZoomCamera.cs
@@ -0,0 +1,46 @@
+﻿#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
+#define USE_INPUT_SYSTEM
+    using UnityEngine.InputSystem;
+    using UnityEngine.InputSystem.Controls;
+#endif
+
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace UnityTemplateProjects
+{
+    public class ZoomCamera : MonoBehaviour
+    {
+        public int minimumZoom, maximumZoom;
+
+        public float zoomAmount = 1.0f;
+
+        [SerializeField]
+        private GameObject floor;
+
+        private void Update()
+        {
+            float scrollAmount = Input.GetAxis("Mouse ScrollWheel");
+
+            // Negative strength when zooming out
+            float zoomStrength = scrollAmount * zoomAmount;
+
+            Vector3 newPosition = transform.forward * zoomStrength;
+            
+            float floorDistance = transform.position.y - floor.transform.position.y;
+
+            bool positive = zoomStrength > 0;
+
+            // Zooming in
+            if (positive && floorDistance - zoomStrength > minimumZoom)
+            { 
+                transform.position += newPosition;
+            }
+            if (!positive && floorDistance + zoomStrength < maximumZoom)
+            {
+                transform.position += newPosition;
+            }
+        }
+    }
+
+}
